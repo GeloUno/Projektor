3d143dab4db758657e2b3918e1b38588a968454c
krasowskiM <krasowski.m87@gmail.com>
2019-02-13 20:44:23 +0100

    Rozszerzenie listy serwerów stun

--- src/app/jsf/videoRTC.js
+++ src/app/jsf/videoRTC.js
@@ -7,7 +7,24 @@ class videoRTC {
         var remoteVideo;
         var iceCandidates = [];
         var peerConnectionConfig = {
-            iceServers: [{ urls: 'stun:stun.l.google.com:19302?transport=udp' },
+            iceServers: [{
+                urls: [
+                    "stun.l.google.com:19302",
+                    "stun1.l.google.com:19302",
+                    "stun2.l.google.com:19302",
+                    "stun3.l.google.com:19302",
+                    "stun4.l.google.com:19302",
+                    "stun.ekiga.net",
+                    "stun.ideasip.com",
+                    "stun.rixtelecom.se",
+                    "stun.schlund.de",
+                    "stun.stunprotocol.org:3478",
+                    "stun.voiparound.com",
+                    "stun.voipbuster.com",
+                    "stun.voipstunt.com",
+                    "stun.voxgratia.org"
+                ]
+            },
             { urls: 'turn:numb.viagenie.ca:3478?transport=udp', username: 'macris120@gmail.com', credential: 'admin1' }]
         };
         var peerConnection = new RTCPeerConnection(peerConnectionConfig);
